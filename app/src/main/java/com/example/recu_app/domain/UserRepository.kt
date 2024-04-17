d7aa79bca79d370700bd42f9635a49e4c7d060cf
manumln <mmolruib@g.educaand.es>
2024-04-17 21:39:05 +0200

    Tarea 3 v2

--- app/src/main/java/com/example/recu_app/domain/UserRepository.kt
+++ app/src/main/java/com/example/recu_app/domain/UserRepository.kt
@@ -4,9 +4,6 @@ import com.example.recu_app.domain.models.User
 import com.example.recu_app.data.users.mem.service.UserService
 
 class UserRepository {
-    fun loadAllUsers(): List<User> {
-        return UserService.getAllUsers()
-    }
 
     fun login(username: String, password: String): User? {
         return UserService.login(username, password)
